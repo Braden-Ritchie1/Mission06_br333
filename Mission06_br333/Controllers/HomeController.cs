bb406531f48b8ddc2b28b394e46f0bfd3f5975bc
Braden Ritchie <br333@byu.edu>
2023-02-11 21:40:26 -0700

    Add project files.

---
+++ Mission06_br333/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission06_br333.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_br333.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //This action displays the Podcast Page
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        //These 2 actions display the Movie Form Page
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(FormResponse fr)
+        {
+            return View("Confirmation");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission06_br333/Models/FormResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_br333.Models
+{
+    public class FormResponse
+    {
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public short Year { get; set; }
+
+        [Required]
+        public string Director { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        public string Lent_to { get; set; }
+
+        public bool Edited { get; set; }
+
+        [MaxLength(25)]
+        public string Notes { get; set; }
+    }
+}
